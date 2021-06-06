372f5cd7e4768439ff7bc058f4fed9a22d06b54d
RicardoFuentes97 <61329449+RicardoFuentes97@users.noreply.github.com>
2021-06-06 15:56:51 -0600

    Gramatica XML

--- src/clases/Analizar.ts
+++ src/clases/Analizar.ts
@@ -7,7 +7,8 @@ export class Analizador {
         console.log("vamos a analizar la entrada");
      
             let ast= sintactico.parse(input);
-            let controlador = new Controlador();
+            
+            /*let controlador = new Controlador();
             let ts_globla =new TablaSimbolos(null);
 
             ast.ejecutar(controlador,ts_globla);
@@ -15,9 +16,9 @@ export class Analizador {
             controlador.graficarEntornos(controlador,ts_globla,"");
             let ts_html =controlador.graficar_ts(controlador,ts_globla);
             let error_html = controlador.graficar_Semantico (controlador,ts_globla);
-            let retorno = {"errores":error_html,"ts": ts_html ,"consola":controlador.consola };
+            let retorno = {"errores":error_html,"ts": ts_html ,"consola":controlador.consola };*/
 
-            return retorno;
+            //return retorno;
         
     }
     public recorrer(input){
